15fde059aa3d18bda1af68bef1f915ae34a5f3cb
ShivamSanap <shivamsanap08@gmail.com>
2025-08-24 14:15:43 +0530

    updated grouping of fg processes and killing based on group id

--- custom_signal_handling.c
+++ custom_signal_handling.c
@@ -3,120 +3,144 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
 
 #define MAX_INPUT_SIZE 1024
 #define MAX_TOKEN_SIZE 64
 #define MAX_NUM_TOKENS 64
+#define MAX_BG_PROCESSES 64
+
+pid_t fg_pid = -1;
 
 char **tokenize(char *line)
 {
-	char **tokens = (char **)malloc(MAX_NUM_TOKENS * sizeof(char *));
-	char *token = (char *)malloc(MAX_TOKEN_SIZE * sizeof(char));
-	int i, tokenIndex = 0, tokenNo = 0;
+    char **tokens = (char **)malloc(MAX_NUM_TOKENS * sizeof(char *));
+    char *token = (char *)malloc(MAX_TOKEN_SIZE * sizeof(char));
+    int i, tokenIndex = 0, tokenNo = 0;
 
-	for(i =0; i < strlen(line); i++)
-	{
-		char readChar = line[i];
-		if (readChar == ' ' || readChar == '\n' || readChar == '\t')
-		{
-			token[tokenIndex] = '\0';
-			if (tokenIndex != 0)
-			{
-				tokens[tokenNo] = (char*)malloc(MAX_TOKEN_SIZE*sizeof(char));
-				strcpy(tokens[tokenNo++], token);
-				tokenIndex = 0; 
-			}
-		} 
-		else 
-		{
-			token[tokenIndex++] = readChar;
-		}
-	}
-	
-	free(token);
-	tokens[tokenNo] = NULL ;
-	return tokens;
+    for (i = 0; i < strlen(line); i++)
+    {
+        char readChar = line[i];
+        if (readChar == ' ' || readChar == '\n' || readChar == '\t')
+        {
+            token[tokenIndex] = '\0';
+            if (tokenIndex != 0)
+            {
+                tokens[tokenNo] = (char *)malloc(MAX_TOKEN_SIZE * sizeof(char));
+                strcpy(tokens[tokenNo++], token);
+                tokenIndex = 0;
+            }
+        }
+        else
+        {
+            token[tokenIndex++] = readChar;
+        }
+    }
+
+    free(token);
+    tokens[tokenNo] = NULL;
+    return tokens;
 }
 
-int main(int argc, char* argv[]) 
+void handler(int sig)
 {
-	char  line[MAX_INPUT_SIZE];            
-	char  **tokens;              
-	int i;
+    kill(-fg_pid, SIGKILL);
+}
 
-	while(1) 
-	{			
-		bzero(line, sizeof(line));
+int main(int argc, char *argv[])
+{
+    char line[MAX_INPUT_SIZE];
+    char **tokens;
+    int i;
+    int bg_process = 0;
+    int bg_pid[MAX_BG_PROCESSES];
 
-		char cwd[256];
-		if (getcwd(cwd, sizeof(cwd)) != NULL) {
-    		printf("%s $ ", cwd);
-		} else {
-			perror("getcwd error");
-			printf("$ ");
-		}
+    signal(SIGINT, handler);
 
-		scanf("%[^\n]", line);
-		getchar();
+    while (1)
+    {
+        bzero(line, sizeof(line));
 
-		printf("Command entered: %s (remove this debug output later)\n", line);
-
-		line[strlen(line)] = '\n'; 
-		tokens = tokenize(line);
-
-        while(waitpid(-1, NULL, WNOHANG)>0)
+        char cwd[256];
+        if (getcwd(cwd, sizeof(cwd)) != NULL)
+            printf("%s $ ", cwd);
+        else
         {
-            printf("bg process reaped");
+            perror("getcwd error");
+            printf("$ ");
         }
 
-		int count=0;
+        scanf("%[^\n]", line);
+        getchar();
 
-        for(i=0;tokens[i]!=NULL;i++)
+        printf("Command entered: %s (remove this debug output later)\n", line);
+
+        line[strlen(line)] = '\n';
+        tokens = tokenize(line);
+
+        int proc_done;
+        while ((proc_done = waitpid(-1, NULL, WNOHANG)) > 0)
+        {
+            printf("bg process %d reaped", proc_done);
+
+            for (int k = 0; k < bg_process; k++)
+            {
+                if (proc_done == bg_pid[k])
+                {
+                    bg_pid[k] = bg_pid[--bg_process];
+                    break;
+                }
+            }
+        }
+
+        int count = 0;
+        for (i = 0; tokens[i] != NULL; i++)
         {
             printf("found token %s (remove this debug output later)\n", tokens[i]);
-            count+=1;
+            count++;
         }
         printf("%d\n", count);
 
-        if(strcmp(tokens[0],"exit")==0)
+        if (strcmp(tokens[0], "exit") == 0)
         {
             break;
         }
-   
-		if(strcmp((tokens[count-1]),"&")==0)
+
+        if (strcmp(tokens[count - 1], "&") == 0)
         {
-            tokens[count-1]=NULL;
+            tokens[count - 1] = NULL;
+
             if (tokens[0] != NULL && strcmp(tokens[0], "cd") == 0)
             {
                 if (tokens[1] == NULL)
-                {
                     fprintf(stderr, "cd: missing argument\n");
-                }
                 else if (tokens[2] != NULL)
-                {
                     fprintf(stderr, "cd: too many arguments\n");
-                }
-                else
-                {
-                    if (chdir(tokens[1]) != 0)
-                    {
-                        perror("cd failed");
-                    }
-                }
+                else if (chdir(tokens[1]) != 0)
+                    perror("cd failed");
+
                 continue;
             }
+
             int pid = fork();
+
             if (pid < 0)
             {
                 perror("Fork failed");
                 exit(1);
             }
 
-            else if(pid == 0)
+            else if (pid == 0)
             {
+                setpgid(0, 0);
                 execvp(tokens[0], tokens);
                 perror("exec failed");
                 exit(1);
+            }
+            else
+            {
+                bg_pid[bg_process] = pid;
+                bg_process++;
             }
         }
         else
@@ -124,23 +148,15 @@ int main(int argc, char* argv[])
             if (tokens[0] != NULL && strcmp(tokens[0], "cd") == 0)
             {
                 if (tokens[1] == NULL)
-                {
                     fprintf(stderr, "cd: missing argument\n");
-                }
                 else if (tokens[2] != NULL)
-                {
                     fprintf(stderr, "cd: too many arguments\n");
-                }
-                else
-                {
-                    if (chdir(tokens[1]) != 0)
-                    {
-                        perror("cd failed");
-                    }
-                }
+                else if (chdir(tokens[1]) != 0)
+                    perror("cd failed");
+
                 continue;
             }
-           
+
             int pid = fork();
 
             if (pid < 0)
@@ -149,7 +165,7 @@ int main(int argc, char* argv[])
                 exit(1);
             }
 
-            else if(pid == 0)
+            else if (pid == 0)
             {
                 execvp(tokens[0], tokens);
                 perror("exec failed");
@@ -158,39 +174,37 @@ int main(int argc, char* argv[])
 
             else
             {
+                fg_pid = pid;
                 int status;
                 wait(&status);
                 if (WIFEXITED(status))
                 {
                     int exit_code = WEXITSTATUS(status);
-                        if (exit_code == 1)
-                        {
-                            printf("EXITSTATUS: %d\n", exit_code);
-                        }
+                    if (exit_code == 1)
+                        printf("EXITSTATUS: %d\n", exit_code);
                 }
             }
         }
-		
-		for(i=0;tokens[i]!=NULL;i++)
-		{
-			free(tokens[i]);
-		}
-		free(tokens);
-	}
 
-    for (int j = 0; j < bg_process; j++) 
+        for (i = 0; tokens[i] != NULL; i++)
+            free(tokens[i]);
+        free(tokens);
+    }
+
+    for (int j = 0; j < bg_process; j++)
     {
-        if (kill(bg_pid[j], SIGTERM) == 0) 
+        if (kill(bg_pid[j], SIGTERM) == 0)
         {
-            printf("%d",bg_pid[j]);
+            printf("%d", bg_pid[j]);
             printf("Killed background process %d\n", bg_pid[j]);
-            waitpid(bg_pid[j], NULL, 0); // reap it
+            waitpid(bg_pid[j], NULL, 0);
         }
-        else 
+        else
         {
             perror("error in killing bg process");
-        }    
+        }
     }
+
     printf("Exiting the shell....\n");
-	return 0;
+    return 0;
 }
